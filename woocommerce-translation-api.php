aa57cb5302acd640c11fc47e03920de07594208d
Qoldn <qoldn2@gmail.com>
2025-10-04 17:02:34 +0300

    Update woocommerce-translation-api.php

--- woocommerce-translation-api.php
+++ woocommerce-translation-api.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WooCommerce Translation API
  * Description: Translate WooCommerce products via OpenAI or DeepL with WPML integration.
- * Version: 1.1.0
+ * Version: 1.2.0
  * Author: Qoldn
  * License: GPL2+
  * Text Domain: wc-translation-api
@@ -23,4 +23,21 @@ add_action('plugins_loaded', function() {
     new WC_Translation_Meta_Box();
     new WC_Translation_Service();
     new WC_Translation_Bulk();
+});
+
+// Enqueue admin JS
+add_action('admin_enqueue_scripts', function($hook) {
+    if ($hook === 'post.php' || $hook === 'post-new.php' || $hook === 'settings_page_wc_translation_api') {
+        wp_enqueue_script(
+            'wc-translation-api-admin',
+            WC_TRANSLATION_API_URL . 'assets/js/admin.js',
+            ['jquery'],
+            '1.2.0',
+            true
+        );
+        wp_localize_script('wc-translation-api-admin', 'wcTranslationApi', [
+            'ajax_url' => admin_url('admin-ajax.php'),
+            'nonce'    => wp_create_nonce('wc_translation_api_nonce'),
+        ]);
+    }
 });
